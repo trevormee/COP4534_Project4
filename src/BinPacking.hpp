a9d3fc99e7a6b5f70eeef36880596b04875b5520
Trevor Mee <122403539+trevormee@users.noreply.github.com>
2024-11-04 19:14:15 +0000

    Makefile, BinPacking.hpp, BinPacking.cpp, main.cpp Initial Setup

---
+++ src/BinPacking.hpp
@@ -0,0 +1,19 @@
+/***************************************************************
+  Student Name: Trevor Mee
+  File Name: AdjacencyMatrix.hpp
+  Project 4
+
+  @brief 
+***************************************************************/
+
+#ifndef BINPACKING_HPP
+#define BINPACKING_HPP
+
+class BinPacking
+{
+    private:
+       
+    public:
+
+};
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello project 4" << std::endl;
+    return 0;
+}
